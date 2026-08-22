36a58634a86a90f76cae49404b3f495e9fbdc58a
a-bzzzz <36167014+a-bzzzz@users.noreply.github.com>
2026-08-22 23:49:18 +0300

    Init models.py

--- CSB-Project1/rbook/models.py
+++ CSB-Project1/rbook/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User, Recipes
+
+class Recipe(models.Model):
+	owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	recipe = models.TextField()
