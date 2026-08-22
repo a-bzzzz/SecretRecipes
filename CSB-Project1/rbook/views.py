b08965eaa9137e59deffbb8f5436de70587b9d43
a-bzzzz <36167014+a-bzzzz@users.noreply.github.com>
2026-08-22 23:03:56 +0300

    Fix views.py init

--- CSB-Project1/rbook/views.py
+++ CSB-Project1/rbook/views.py
@@ -1,8 +1,21 @@
-from django.urls import path
+from django.shortcuts import render
 
-from .views import homePageView, addView
+@login_required
+def addView(request):
 
-urlpatterns = [
-    path('', homePageView, name='home'),
-    path('add/', addView, name='add'),
-]
+	if request.method == 'POST':
+		new_recipe = request.POST.get('recipes')
+		user = request.user
+
+		if new_recipe:
+			Recipes.objects.create(id=id, rname=rname, category=category, secret=secret, portions=portions, ingredients=ingredients, guidance=guidance)
+
+		return redirect('/')
+
+	return redirect('/')
+
+@login_required
+def homePageView(request):
+	items = request.session.get('recipes', [])		
+
+	return render(request, 'templates/index.html', {'recipes' : recipes})
