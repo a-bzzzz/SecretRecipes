7ee774deced841cfd7ecd68317be5e7127bafef4
a-bzzzz <36167014+a-bzzzz@users.noreply.github.com>
2026-08-23 15:31:50 +0300

    Indentation fix

--- CSB-Project1/rbook/models.py
+++ CSB-Project1/rbook/models.py
@@ -18,14 +18,14 @@ class Recipe(models.Model):
 	owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
 
 	category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='main_dish')
-    secret = models.BooleanField(default=False)
-    portions = models.IntegerField(default=1)
+	secret = models.BooleanField(default=False)
+	portions = models.IntegerField(default=1)
 	
 	# Simplified input feed with a new line, eg.
-    # - 3 dl milk
-    # - 2 tbsp cocoa powder
-    ingredients = models.TextField(help_text="Enter each amount & ingredient on a new line")
-    guidance = models.TextField(help_text="Enter preparation steps on new lines")
+	# - 3 dl milk
+	# - 2 tbsp cocoa powder
+	ingredients = models.TextField(help_text="Enter each amount & ingredient on a new line")
+	guidance = models.TextField(help_text="Enter preparation steps on new lines")
 
 def __str__(self):
         return self.rname
