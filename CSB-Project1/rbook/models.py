59299ffae888b87fee8da26a438186d5f76cee8f
a-bzzzz <36167014+a-bzzzz@users.noreply.github.com>
2026-08-23 15:48:48 +0300

    Add again missing 'rname' field

--- CSB-Project1/rbook/models.py
+++ CSB-Project1/rbook/models.py
@@ -17,6 +17,7 @@ CATEGORY_CHOICES = [
 class Recipe(models.Model):
 	owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
 
+	rname = models.CharField(max_length=100)
 	category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='main_dish')
 	secret = models.BooleanField(default=False)
 	portions = models.IntegerField(default=1)
