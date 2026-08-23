4bb20c323b8076db5cf9a9322f3bd7748cf2b9e1
a-bzzzz <36167014+a-bzzzz@users.noreply.github.com>
2026-08-23 22:51:22 +0300

    Add path to 'add_recipe' view

--- CSB-Project1/rbook/urls.py
+++ CSB-Project1/rbook/urls.py
@@ -6,9 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('recipe/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
     path('search/', views.search, name='search'),
+    path('add/', views.add_recipe, name='add_recipe'),
     path('register/', views.register, name='register'),
-    
-    # Django login & logout
     path('login/', auth_views.LoginView.as_view(template_name='rbook/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
