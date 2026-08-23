70962391b9658719cc0d1e6e00baaa2eaa0aa511
a-bzzzz <36167014+a-bzzzz@users.noreply.github.com>
2026-08-23 14:27:42 +0300

    Update models.py

--- CSB-Project1/rbook/models.py
+++ CSB-Project1/rbook/models.py
@@ -1,7 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-from django.contrib.auth.models import User, Recipes
+# Recipe categories for drop-down list
+CATEGORY_CHOICES = [
+	('breakfast', 'Breakfast'),
+	('first_dish', 'Appetizers'),
+	('soup', 'Soups'),
+	('salad', 'Salads'),
+	('main_dish', 'Main Dishes'),
+	('side_dish', 'Side Dishes'),
+	('dessert', 'Desserts'),
+	('drink', 'Drinks'),
+	('bake', 'Baking'),
+]
 
 class Recipe(models.Model):
-	owner = models.ForeignKey(User, on_delete=models.CASCADE)
-	recipe = models.TextField()
+	owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
+
+	category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='main_dish')
+    secret = models.BooleanField(default=False)
+    portions = models.IntegerField(default=1)
+	
+	# Simplified input feed with a new line, eg.
+    # - 3 dl milk
+    # - 2 tbsp cocoa powder
+    ingredients = models.TextField(help_text="Enter each amount & ingredient on a new line")
+    guidance = models.TextField(help_text="Enter preparation steps on new lines")
+
+def __str__(self):
+        return self.rname
