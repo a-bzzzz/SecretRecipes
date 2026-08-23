2c3783ccf5bb935946ed26b46842bdc6aebc03a9
a-bzzzz <36167014+a-bzzzz@users.noreply.github.com>
2026-08-23 19:18:00 +0300

    Fix import get_object_or_response to get_object_or_404 in rbook/views.py

--- CSB-Project1/rbook/views.py
+++ CSB-Project1/rbook/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_response, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.db import connection
 from .models import Recipe
 
