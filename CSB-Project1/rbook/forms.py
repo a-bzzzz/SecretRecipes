157f972107764ec8c16821599651bbaf34aec885
a-bzzzz <36167014+a-bzzzz@users.noreply.github.com>
2026-08-24 19:51:07 +0300

    Update security flaw

--- CSB-Project1/rbook/forms.py
+++ CSB-Project1/rbook/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import Recipe
+from django.contrib.auth.password_validation import validate_password
 
 class RegisterForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -9,12 +10,21 @@ class RegisterForm(forms.ModelForm):
         model = User
         fields = ['username', 'password']
 
-    # FLAW 3: A07:2025 - Identification and Authentication Failures
-    # VULNERABLE VERSION: Bypass Django's password checks (validate_password)
-    # and store the password directly without any strength requirements.
+    # FLAW 3: A07:2025 - Authentication Failures (CWE-521: Weak Password Requirements)
+    
     def save(self, commit=True):
         user = super().save(commit=False)
+        raw_password = self.cleaned_data["password"]
+
+        # VULNERABILITY 3: Bypass Django's password checks (validate_password)
+        # and store the password directly without any strength requirements.
         user.set_password(self.cleaned_data["password"])
+
+        # FIX 3: Enforce standard Django password validation
+        # validate_password(raw_password, user)
+        
+        user.set_password(raw_password)
+        
         if commit:
             user.save()
         return user
