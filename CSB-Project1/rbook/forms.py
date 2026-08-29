721bced24578ec841c60f50e95611318e1a5eb60
Asta Björk <asta.bjork@helsinki.fi>
2026-08-29 03:04:34 +0300

    Put Flaw 3 back

--- CSB-Project1/rbook/forms.py
+++ CSB-Project1/rbook/forms.py
@@ -5,27 +5,27 @@ from django.contrib.auth.forms import UserCreationForm
 
 # FLAW 3: A07:2025 - Authentication Failures 
 # (CWE-521: Weak Password Requirements)
-# class RegisterForm(forms.ModelForm):
-#     password = forms.CharField(widget=forms.PasswordInput)
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
 
-#     class Meta:
-#         model = User
-#         fields = ['username', 'password']
+    class Meta:
+        model = User
+        fields = ['username', 'password']
 
-#     # VULNERABILITY 3: Saves password without any length or complexity checks
-#     def save(self, commit=True):
-#         user = super().save(commit=False)
-#         user.set_password(self.cleaned_data["password"])
+    # VULNERABILITY 3: Saves password without any length or complexity checks
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
         
-#         if commit:
-#             user.save()
-#         return user
+        if commit:
+            user.save()
+        return user
         
 # FIX 3: Use Django's built-in UserCreationForm, 
 # which enforces strong password validation policies (CWE-521).
-class RegisterForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        fields = ("username",)
+# class RegisterForm(UserCreationForm):
+#     class Meta(UserCreationForm.Meta):
+#         fields = ("username",)
         
 
 class RecipeForm(forms.ModelForm):
